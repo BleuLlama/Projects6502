ac83888dc883526bf783f0f3f320c3a3305c68a2
Scott Lawrence <yorgle@gmail.com>
2016-01-24 16:40:06 -0500

    Working and updated documentation

--- Fibonacci/fibonacci.asm
+++ Fibonacci/fibonacci.asm
@@ -6,6 +6,9 @@
 ; Scott Lawrence - yorgle@gmail.com
 
 
+; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; 
+; Configuration
+
 VERSIONL = 0
 VERSIONH = 1
 
@@ -17,77 +20,157 @@ UseVideoDisplay0 = 1
 .include "KimCode200.asm"
 .include "KimLib.asm"
 
-;; a little bit of ram we use
 
-; Value   LL MM RR
-VALL	= $20
-VALM	= $21
-VALR	= $22
+; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; 
+; RAM
 
-; 0 1 1 2 3 5 ...
+; the operations stack is:
+;	display	param0	param1
+;	RESULT	VALI	VALJ
+VALI	 = $20		; Parameter 1 for operations (24 bit)
+VALJ	 = $23		; Parameter 2 for operations (24 bit)
+RESULT 	 = $26		; Result for operations (24 bit)
+OVERFLOW = $1F		; if operation overflowed, this is set
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-; Do the thing...
+
+; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; 
+; main
+;	the main loop
 main:
+setup:
 	jsr	cls		; clear the video display black
 
 	; clear the value, setup the 
 	lda	#$0		; A = 0
-	sta	VALL		; VALL = 00
-	sta	VALM		; VALM =   00
-	sta	VALR		; VALR =      00
+	sta	RESULT+0	; RESULT =     00
+	sta	RESULT+1	; RESULT =   00
+	sta	RESULT+2	; RESULT = 00
+
+	sta	VALI+0		; VALI =       00
+	sta	VALI+1		; VALI =    00
+	sta	VALI+2		; VALI = 00
+
+	sta	VALJ+0		; VALJ =       00
+	sta	VALJ+1		; VALJ =    00
+	sta	VALJ+2		; VALJ = 00
 
 	cld			; HEX mode
 	sed			; Decimal mode (BCD)
 
 ; since the Fib sequence starts with two values that don't follow
 ; the pattern (0, 1)  we need to preload it..
-mainFirstbits:
-	; display 00 00 00
+preloadFibonacci:
+	; prep 00 00 00
 	jsr	displayAndWait	; display to LED, Video Display, wait for press
 
-	; display 00 00 01
+	; prep 00 00 01
 	lda	#$01
-	sta	VALR		; VALR =      01
-	jsr	displayAndWait	; display to LED, Video Display, wait for press
+	sta	RESULT+0	; RESULT = 00 00 01
+	jsr	rollValues	; J gets I, I gets RESULT
 
+; now, once we get going, we can just work from RESULT/I/J
 mainLoop:
 	jsr	displayAndWait	; display to LED, Video Display, wait for press
-	jsr	computeValue	; compute the next value
-	jmp	mainLoop	; and again.
+
+	jsr	addIJ		; RESULT = I + J
+	jsr	rollValues	; J gets I, I gets RESULT
+
+	; check for overflow...
+	lda	OVERFLOW	; see if there was a math overflow 
+	cmp	#$00
+	beq	mainLoop	; nope?  Go again!
+
+	; display overflow error
+	lda	#$EE		; display error
+	sta	KIM_POINTH	; EE
+	sta	KIM_POINTL	;    EE
+	sta	KIM_INH		;       EE
+	jsr	waitForPress	; wait for press
+
+	; now just restart from scratch
+	jmp	setup
 
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-; compute the next value
-computeValue:
-	clc			; clear carry
-	lda	VALR		; A = VALR
-	adc	VALR		; A = VALR + VALR
-	sta	VALR		; (store it)
+; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; 
+; math operations
 
-; nope
-	lda	#$0		; A = 0
-	adc	VALM		; A = 0 + VALM + C
-	sta	VALM		; (store it)
+; rollValues
+;	J gets I
+; 	I gets RESULT
+;	RESULT is untouched
+rollValues:
+	LDA	VALI+0		; J gets I
+	STA	VALJ+0
+	LDA	VALI+1
+	STA	VALJ+1
+	LDA	VALI+2
+	STA	VALJ+2
 
-	lda	#$0		; A = 0
-	adc	VALR		; A = VALL + C
-	sta	VALR		; (store it)
+	LDA	RESULT+0	; I gets RESULT
+	STA	VALI+0
+	LDA	RESULT+1
+	STA	VALI+1
+	LDA	RESULT+2
+	STA	VALI+2
 
-	rts			; we're done, return
+	rts;
+
+; addIJ
+;	C is untouched
+;	B is untouched
+;	RESULT gets C+B
+; 	OVERFLOW is set/cleared
+addIJ:
+	jsr	clearOverflow	; clear overflow
+	clc			; C (carry) is cleared
+
+	lda	VALJ+0
+	adc	VALI+0
+	sta	RESULT+0	; RESULT0 = VALI0 + VALJ0 + C(0)
+	lda	VALJ+1
+	adc	VALI+1
+	sta	RESULT+1	; RESULT1 = VALI1 + VALJ1 + C
+	lda	VALJ+2
+	adc	VALI+2
+	sta	RESULT+2	; RESULT2 = VALI2 + VALJ2 + C
+
+	bcs	setOverflow	; set overflow if C was set
+	rts
+
+; clearOverflow
+;	OVERFLOW is set to 0
+clearOverflow:
+	lda	#$00
+	sta	OVERFLOW	; OVERFLOW = 0
+	rts
+
+	
+; setOverflow
+;	OVERFLOW is set to 1
+setOverflow:
+	lda	#$01
+	sta	OVERFLOW	; OVERFLOW = 1
+	rts
 
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-; display value and wait for press
+
+; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; 
+; input and display operations
+
+; displayAndWait
+;	display the result to the LEDs
+;	display the result to the video display
+;	wait for any key to be hit on the keypad
 displayAndWait:
-	jsr	displayValue	; display VALx to the LED display
-	jsr	displayColor	; display VALx to the video display
+	jsr	displayResult	; display the result to the LED display
+	jsr	displayColor	; display the result to the video display
 	jsr	waitForPress	; wait for the user to press something
 	rts
 
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-; wait for a user press
+; waitForPress
+;	wait for the user to hit a key
+;	A gets the key pressed
 waitForPress:
 	jsr	SCANDS		; refresh the display (for real KIM)
 	jsr	GETKEY		; get a keypad press
@@ -96,22 +179,23 @@ waitForPress:
 	rts			; we're done, return.
 
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-; display the value to the LED display
-displayValue:
-	lda	VALL
-	sta	KIM_POINTH	; left two digits
-	lda	VALM
-	sta	KIM_POINTL	; left two digits
-	lda	VALR
-	sta	KIM_INH		; left two digits
+; displayResult
+;	Copy the RESULT to the KIM display ram
+;	display it to the LEDs
+displayResult:
+	lda	RESULT+2
+	sta	KIM_POINTH	; xx .. ..
+	lda	RESULT+1
+	sta	KIM_POINTL	; .. xx ..
+	lda	RESULT+0
+	sta	KIM_INH		; .. .. xx
 
 	jsr	SCANDS		; refresh it to the display
 	rts			; we're done, return.
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-; display the value as a color
+; displayColor
+;	fill video filled with the color from the bottom nib of RESULT
 displayColor:
-	lda	VALR		; set the color (bottom nibble)
+	lda	RESULT+0	; set the color (bottom nibble)
 	jsr	fillscr		; fill the screen
 	rts			; we're done, return.
